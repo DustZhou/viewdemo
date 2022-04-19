68558b92ad5a8880a359477442482a60d8884d49
刘渝 <liuyu1@hudunsoft.com>
2022-04-19 18:34:57 +0800

    优化  优化代码，删除冗余代码

--- app/src/main/java/com/fengsu/levelview/LevelView.java
+++ app/src/main/java/com/fengsu/levelview/LevelView.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.hardware.Sensor;
